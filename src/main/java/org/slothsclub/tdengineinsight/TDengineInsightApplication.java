19b5899f559cd4de57669bc11962fc85a600b508
Mona Lisa <monlisa@lisa.local>
2023-06-07 16:18:05 +0800

    first commit

---
+++ src/main/java/org/slothsclub/tdengineinsight/TDengineInsightApplication.java
@@ -0,0 +1,13 @@
+package org.slothsclub.tdengineinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDengineInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TDengineInsightApplication.class, args);
+    }
+
+}
