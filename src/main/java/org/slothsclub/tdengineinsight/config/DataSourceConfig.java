bf2db9f9638f7bcf98c5074ff125a63eefd8054a
Forrest <1168948+ForrestRolf@users.noreply.github.com>
2023-06-21 15:18:18 +0800

    Remove custom sql session factory

--- src/main/java/org/slothsclub/tdengineinsight/config/DataSourceConfig.java
+++ src/main/java/org/slothsclub/tdengineinsight/config/DataSourceConfig.java
@@ -1,12 +1,10 @@
 package org.slothsclub.tdengineinsight.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -14,14 +12,6 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dynamicDataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dynamicDataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mappings/**/*.xml"));
-        return bean.getObject();
-    }
 
     @Bean(name = "sqlSessionTemplate")
     public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
