48e0371504d78ffde8830846116bfb67d56c59c2
Forrest <1168948+ForrestRolf@users.noreply.github.com>
2023-06-27 15:27:21 +0800

    Enable CORS

---
+++ src/main/java/org/slothsclub/tdengineinsight/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.slothsclub.tdengineinsight.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+    @Bean
+    @ConditionalOnProperty(value = "app.cors.enabled", havingValue = "true", matchIfMissing = false)
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
